ee808a537bc6ad4ba1a3d4eda0a45d5ea71df470
bagusdewantoro <bagusdewantoro86@gmail.com>
2022-04-10 00:04:51 +0700

    remove node_modules

---
+++ datastructure.js
@@ -0,0 +1,17 @@
+const Stack = require('./stack');
+
+// const stack = new Stack;
+// stack.push(2)
+// stack.push(3)
+// stack.clear()
+// stack.push(10)
+// stack.push(11)
+// stack.push(12)
+// console.log(stack.pop())
+// stack.push(30)
+// console.log(stack)
+// console.log(stack.peek())
+// console.log(stack.size())
+// console.log(stack.toString()) // only this method will have O(n)
+
+module.exports = Stack;
